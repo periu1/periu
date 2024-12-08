ea045ea2555aa65d992628c5c5d7a6d6fc330d54
periu1 <periugod@gmail.com>
2024-12-08 15:22:24 +0300

    Add files via upload

---
+++ дз 4/1/дз 5/my.js
@@ -0,0 +1,9 @@
+        let name="Lesha";
+        res= name.match(/\S/g);
+        console.log(res);
+        const number="12345";
+        let rezult=1;
+        for(let i=0; i<=number.length - 1;i++){
+            rezult*=(number[i]);
+        }
+        alert(rezult);3
